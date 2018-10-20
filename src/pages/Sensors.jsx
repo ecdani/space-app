d32407770033efdc0303ba22bfb66659dc243be8
ecdani <ec.dani@gmail.com>
2018-10-20 14:00:24 +0200

    Transparencia

--- src/pages/Sensors.jsx
+++ src/pages/Sensors.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default () => (
   <div>
-    <img className="img-mars" src="../src/assets/img/mars.jpg" alt="mars" />
+    <img className="img-mars" src="../src/assets/img/mars.png" alt="mars" />
   </div>
 );
