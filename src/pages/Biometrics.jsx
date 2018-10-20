fbd44e8cada17e23fed549366a135130daf8f7d1
Flopi <flopivillagarcia@gmail.com>
2018-10-20 20:30:54 +0200

    .

--- src/pages/Biometrics.jsx
+++ src/pages/Biometrics.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import AtmosphericComposition from '../components/AtmosphericComposition';
 import Humidity from '../components/Humidity';
 
-export default () => (<div className="biometrics-page">
-  <h1 className="Name">Mark Watney</h1>
-  <div className="Image">
+export default () => (
+  <div className="biometrics-page">
+    <h1 className="Name">Mark Watney</h1>
+    <div className="Image">
       <span className="helper" />
-      <img className="img-bio" src="../src/assets/img/biometrics.jpg" alt="bio" />
+      <img
+        className="img-bio"
+        src="../src/assets/img/biometrics.jpg"
+        alt="bio"
+      />
     </div>
-  <div className="Radiacion-start-cell" />
-  <div className="Sidebar-1" />
-  <div className="Sidebar-2">
+    <div className="Radiacion-start-cell" />
+    <div className="Sidebar-1" />
+    <div className="Sidebar-2">
       <Humidity percent={45} />
     </div>
-  <div className="Sidebar-3">
+    <div className="Sidebar-3">
       <AtmosphericComposition />
     </div>
-</div>
-
+  </div>
 );
