0b39e2b04d60c81c7436b218da85d6225d51996f
Flopi <flopivillagarcia@gmail.com>
2018-10-21 00:36:29 +0200

    set active items in nav

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, withRouter } from 'react-router-dom';
 import Biometrics from './pages/Biometrics';
 import Comunications from './pages/Comunications';
 import Navigation from './pages/Navigation';
@@ -7,24 +7,63 @@ import Sensors from './pages/Sensors';
 import FooterRow from './components/FooterRow';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: '' };
+
+    this.changePage = this.changePage.bind(this);
+  }
+
+  changePage(page) {
+    const { activeTab } = this.state;
+
+    if (activeTab !== page) this.setState({ activeTab: page });
+  }
+
   render() {
+    const { activeTab } = this.state;
+
     return (
       <div>
         <div className="container-flex menu-container">
           <div className="row menu-row">
-            <Link to="/biometrics" className="btn btn-primary btn-first">
+            <Link
+              to="/biometrics"
+              className={`btn btn-primary btn-first ${
+                activeTab === 'biometrics' ? 'btn-active' : ''
+              }`}
+              onClick={() => this.changePage('biometrics')}
+            >
               Biometrics
             </Link>
 
-            <Link to="/comunications" className="btn btn-primary btn-main">
+            <Link
+              to="/comunications"
+              className={`btn btn-primary btn-main  ${
+                activeTab === 'comunications' ? 'btn-active' : ''
+              }`}
+              onClick={() => this.changePage('comunications')}
+            >
               Comunications
             </Link>
 
-            <Link to="/navigation" className="btn btn-primary btn-main">
+            <Link
+              to="/navigation"
+              className={`btn btn-primary btn-main ${
+                activeTab === 'navigation' ? 'btn-active' : ''
+              }`}
+              onClick={() => this.changePage('navigation')}
+            >
               Navigation
             </Link>
 
-            <Link to="/sensors" className="btn btn-primary btn-last">
+            <Link
+              to="/sensors"
+              className={`btn btn-primary btn-last ${
+                activeTab === 'sensors' ? 'btn-active' : ''
+              }`}
+              onClick={() => this.changePage('sensors')}
+            >
               Sensors
             </Link>
           </div>
--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ const createWindow = async () => {
     width: 854,
     height: 480,
     frame: false,
-    fullscreen: true,
   });
 
   // and load the index.html of the app.
