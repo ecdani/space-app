98ca5c7bb8b340d52c24f792c94930839837dbfe
ecdani <ec.dani@gmail.com>
2018-10-20 14:20:10 +0200

    Poca cosa

--- src/pages/Biometrics.jsx
+++ src/pages/Biometrics.jsx
@@ -1,3 +1,5 @@
 import React from 'react';
 
-export default () => <div>Hello From Biometrics Page</div>;
+export default () => (<div>
+  <img className="img-bio" src="../src/assets/img/biometrics.jpg" alt="bio" />
+</div>);
