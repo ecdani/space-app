562072ee2fcad5c67a5c6bbd57a6669313ef897f
ecdani <ec.dani@gmail.com>
2018-10-21 17:44:33 +0200

    endpoints

--- endpoint.py
+++ endpoint.py
@@ -1,39 +1,48 @@
-
-
 from flask import Flask, request
 from flask_restful import Resource, Api
 #from sqlalchemy import create_engine
 from json import dumps
 from flask import jsonify
+import simplejson as json
+import serial
+import time
 
 #db_connect = create_engine('sqlite:///chinook.db')
 app = Flask(__name__)
 api = Api(app)
 
 class Mbar(Resource):
-    def get(self):
-        return {'mbar': 1} # Fetches first column that is Employee ID
+	def get(self):
+		#try:
+		ser = serial.Serial('/dev/ttyUSB1',115200)
+                	#time.sleep(1)
+                	#print(bufsize)
+		buffer = ser.readline()
+                	#print (buffer)
+		#except json.JSONDecodeError:
+			#print ("Error : try to parse an incomplete message")
+		ser.close
+		return {'mbar': 1} # Fetches first column that is Employee ID
 
 class Tracks(Resource):
-    def get(self):
-        conn = db_connect.connect()
-        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        return jsonify(result)
+	def get(self):
+		conn = db_connect.connect()
+		query = conn.execute("select trackid, name, composer, unitprice from tracks;")
+		result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+		return jsonify(result)
 
 class Employees_Name(Resource):
-    def get(self, employee_id):
-        conn = db_connect.connect()
-        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        return jsonify(result)
-        
+	def get(self, employee_id):
+		conn = db_connect.connect()
+		query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
+		result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+		return jsonify(result)
 
 api.add_resource(Mbar, '/employees') # Route_1
-#api.add_resource(Tracks, '/tracks') # Route_2
+api.add_resource(Tracks, '/tracks') # Route_2
 #api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
 
 
 if __name__ == '__main__':
-     app.run(host= '0.0.0.0', port='5002')
-     
+	app.run(host= '0.0.0.0', port='5005')
+
